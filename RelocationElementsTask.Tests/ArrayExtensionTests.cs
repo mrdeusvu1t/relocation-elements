7b23054d2412869fc4804abf692f27aeef235cfa
Anzhelika Kravchuk <AnzhelikaKravchuk@users.noreply.github.com>
2020-05-06 12:06:51 +0300

    add-task-and-test-projects

---
+++ RelocationElementsTask.Tests/ArrayExtensionTests.cs
@@ -0,0 +1,36 @@
+using System;
+using NUnit.Framework;
+
+namespace RelocationElementsTask.Tests
+{
+    public class ArrayExtensionTests
+    {
+        [TestCase(new int[] {1, 2, 3, 4, 5, 3, 2, 3, 1, 3, 4, 5, 1, 5, 0, 7, 6, 5}, 3,
+            new int[] {1, 2, 4, 5, 2, 1, 4, 5, 1, 5, 0, 7, 6, 5, 3, 3, 3, 3})]
+        [TestCase(new int[] {1, 2, 3, 4, 5, 3, 2, 3, 1, 3, 4, 5, 1, 5, 0, 7, 6, 5}, 1,
+            new int[] {2, 3, 4, 5, 3, 2, 3, 3, 4, 5, 5, 0, 7, 6, 5, 1, 1, 1})]
+        [TestCase(new int[] {1, 2, 3, 4, 5, 3, 2, 3, 1, 3, 4, 5, 1, 5, 0, 7, 6, 5}, 5,
+            new int[] {1, 2, 3, 4, 3, 2, 3, 1, 3, 4, 1, 0, 7, 6, 5, 5, 5, 5})]
+        [TestCase(new int[] {1, int.MinValue, int.MinValue, int.MaxValue, int.MinValue, -1, -3, -9, 1}, int.MinValue,
+            new int[] {1, int.MaxValue, -1, -3, -9, 1, int.MinValue, int.MinValue, int.MinValue})]
+        public void MoveToTailTests(int[] source, int value, int[] expected)
+        {
+            ArrayExtension.MoveToTail(source, value);
+            Assert.AreEqual(expected, source);
+        }
+
+        [Test]
+        public void MoveToTail_Array_Is_Null_Throw_ArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => ArrayExtension.MoveToTail(null, 1),
+                "Array cannot be null.");
+        }
+
+        [Test]
+        public void MoveToTail_Array_Is_Empty_Throw_ArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => ArrayExtension.MoveToTail(new int[] { }, 1),
+                "Array cannot be empty.");
+        }
+    }
+}
---
+++ RelocationElementsTask/ArrayExtension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RelocationElementsTask
+{
+    public static class ArrayExtension
+    {
+        public static void MoveToTail(int[] source, int value)
+        {
+            throw  new NotImplementedException("You need to implement this function.");
+        }
+    }
+}
