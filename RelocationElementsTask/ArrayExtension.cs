1e2e56fe9c8e387cf4d938b389b6d3cded58ba7f
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-05-29 14:56:25 +0000

    Update ArrayExtension.cs

--- RelocationElementsTask/ArrayExtension.cs
+++ RelocationElementsTask/ArrayExtension.cs
@@ -1,9 +1,19 @@
-﻿using System;
+using System;
 
 namespace RelocationElementsTask
 {
+    /// <summary>
+    /// Class for operations with array.
+    /// </summary>
     public static class ArrayExtension
     {
+        /// <summary>
+        /// Moves all of the elements with set value to the end, preserving the order of the other elements.
+        /// </summary>
+        /// <param name="source"> Source array. </param>
+        /// <param name="value">Source value.</param>
+        /// <exception cref="ArgumentNullException">Thrown when array is null.</exception>
+        /// <exception cref="ArgumentException">Thrown when array is empty.</exception>
         public static void MoveToTail(int[] source, int value)
         {
             throw  new NotImplementedException("You need to implement this function.");
