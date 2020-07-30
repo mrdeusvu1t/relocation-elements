1b27d6127fdeda44bc79f40b4903e2e9b1ba9a63
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-07-30 08:51:18 +0000

    Replace ArrayExtensionTests.cs

--- RelocationElementsTask.Tests/ArrayExtensionTests.cs
+++ RelocationElementsTask.Tests/ArrayExtensionTests.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using NUnit.Framework;
 
 #pragma warning disable CA1707
@@ -7,14 +7,10 @@ namespace RelocationElementsTask.Tests
 {
     public class ArrayExtensionTests
     {
-        [TestCase(new int[] {1, 2, 3, 4, 5, 3, 2, 3, 1, 3, 4, 5, 1, 5, 0, 7, 6, 5}, 3,
-            new int[] {1, 2, 4, 5, 2, 1, 4, 5, 1, 5, 0, 7, 6, 5, 3, 3, 3, 3})]
-        [TestCase(new int[] {1, 2, 3, 4, 5, 3, 2, 3, 1, 3, 4, 5, 1, 5, 0, 7, 6, 5}, 1,
-            new int[] {2, 3, 4, 5, 3, 2, 3, 3, 4, 5, 5, 0, 7, 6, 5, 1, 1, 1})]
-        [TestCase(new int[] {1, 2, 3, 4, 5, 3, 2, 3, 1, 3, 4, 5, 1, 5, 0, 7, 6, 5}, 5,
-            new int[] {1, 2, 3, 4, 3, 2, 3, 1, 3, 4, 1, 0, 7, 6, 5, 5, 5, 5})]
-        [TestCase(new int[] {1, int.MinValue, int.MinValue, int.MaxValue, int.MinValue, -1, -3, -9, 1}, int.MinValue,
-            new int[] {1, int.MaxValue, -1, -3, -9, 1, int.MinValue, int.MinValue, int.MinValue})]
+        [TestCase(new int[] { 1, 2, 3, 4, 5, 3, 2, 3, 1, 3, 4, 5, 1, 5, 0, 7, 6, 5 }, 3, new int[] { 1, 2, 4, 5, 2, 1, 4, 5, 1, 5, 0, 7, 6, 5, 3, 3, 3, 3 })]
+        [TestCase(new int[] { 1, 2, 3, 4, 5, 3, 2, 3, 1, 3, 4, 5, 1, 5, 0, 7, 6, 5 }, 1, new int[] { 2, 3, 4, 5, 3, 2, 3, 3, 4, 5, 5, 0, 7, 6, 5, 1, 1, 1 })]
+        [TestCase(new int[] { 1, 2, 3, 4, 5, 3, 2, 3, 1, 3, 4, 5, 1, 5, 0, 7, 6, 5 }, 5, new int[] { 1, 2, 3, 4, 3, 2, 3, 1, 3, 4, 1, 0, 7, 6, 5, 5, 5, 5 })]
+        [TestCase(new int[] { 1, int.MinValue, int.MinValue, int.MaxValue, int.MinValue, -1, -3, -9, 1 }, int.MinValue, new int[] { 1, int.MaxValue, -1, -3, -9, 1, int.MinValue, int.MinValue, int.MinValue })]
         public void MoveToTailTests(int[] source, int value, int[] expected)
         {
             ArrayExtension.MoveToTail(source, value);
@@ -30,7 +26,7 @@ namespace RelocationElementsTask.Tests
         [Test]
         public void MoveToTail_ArrayIsEmpty_ThrowArgumentException()
         {
-            Assert.Throws<ArgumentException>(() => ArrayExtension.MoveToTail(Array.Empty<int>, 1), "Array cannot be empty.");
+            Assert.Throws<ArgumentException>(() => ArrayExtension.MoveToTail(Array.Empty<int>(), 1), "Array cannot be empty.");
         }
     }
 }
